7f06a5b0391deaeacf315d89fdbc8684aa6fab56
Gavin Adams <me@gavinadams.org>
2024-02-18 15:22:40 -0500

    fix: header

--- obrl/oblr-src-result-table-copy.user.js
+++ obrl/oblr-src-result-table-copy.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name          Sim Racer Hub Race Result Table Copy
-// @version       0.1.0
+// @version       0.2.0
 // @description   Copies the result table for easy paste into Excel
 // @author        Gavin Adams
 // @namespace     OBRL
@@ -8,8 +8,8 @@
 // @icon          https://www.google.com/s2/favicons?sz=64&domain=simracerhub.com
 // @grant         GM_registerMenuCommand
 // @grant         GM_setClipboard
-// @updateurl     https://raw.githubusercontent.com/gadams999/user-scripts/main/obrl/oblr-src-result-table-copy.js
-// @downloadurl   https://raw.githubusercontent.com/gadams999/user-scripts/main/obrl/oblr-src-result-table-copy.js
+// @updateURL     https://raw.githubusercontent.com/gadams999/user-scripts/main/obrl/oblr-src-result-table-copy.js
+// @downloadURL   https://raw.githubusercontent.com/gadams999/user-scripts/main/obrl/oblr-src-result-table-copy.js
 // ==/UserScript==
 
 (function () {
@@ -19,7 +19,8 @@
     const tables = document.querySelectorAll(".react-table");
     let table = null;
     let table_text = null;
-    const re = /^(?<header>FIN.*TEAM..)(?<body>.*)$/s;
+    // Raw header is FIN to the first \n\n, body is the rest
+    const re = /^(?<header>FIN.*\n\n)(?<body>.*)$/s;
     let header = "";
     let body = "";
     switch (tables.length) {
@@ -28,9 +29,11 @@
         break;
       case 1:
         table = tables[0];
+        break;
       case 3:
         // 2024-02-16 - SRH shows 3 tables today. This may change.
         table = tables[0];
+        break;
       default:
         if (table === null) {
           const choice = parseInt(
@@ -46,26 +49,26 @@
             return;
           }
         }
-        var x = table.outerText;
-        //console.log(x)
-        table_text = x.match(re);
-        header = table_text.groups.header;
-        body = table_text.groups.body;
-        // header -- replace \n with ' '
-        header = header.replaceAll(/(\w)\n(\w)/g, "$1 $2");
-        // header -- replace \n\t\n with \t
-        header = header.replaceAll(/\n\t\n/g, "\t");
-        // header -- strip final \n
-        header = header.substring(0, header.length - 1);
-        console.log(JSON.stringify(header));
-        // body -- strip final \t
-        body = body.substring(0, body.length - 1);
-
-        GM_setClipboard("".concat(header, body), "text");
-        alert(
-          `Table is propably copied (well, without Fisch's details, lol). Now paste it into spreadsheet.`
-        );
     }
+    var x = table.outerText;
+    // console.log(JSON.stringify(x));
+    table_text = x.match(re);
+    header = table_text.groups.header;
+    body = table_text.groups.body;
+    // header -- replace \n with ' '
+    header = header.replaceAll(/(\w)\n(\w)/g, "$1 $2");
+    // header -- replace \n\t\n with \t
+    header = header.replaceAll(/\n\t\n/g, "\t");
+    // header -- strip final \n
+    header = header.substring(0, header.length - 1);
+    console.log(JSON.stringify(header));
+    // body -- strip final \t
+    body = body.substring(0, body.length - 1);
+
+    GM_setClipboard("".concat(header, body), "text");
+    alert(
+      `Table is probably copied (well, without Fisch's details, lol). Now paste it into spreadsheet.`
+    );
   }
 
   GM_registerMenuCommand("Copy table for spreadsheet", copyTable);
