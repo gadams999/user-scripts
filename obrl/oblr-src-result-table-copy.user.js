5eae5fa37a8f6dc590604cffeaaecbd031a71e73
Gavin Adams <me@gavinadams.org>
2024-02-18 15:32:48 -0500

    feat: detect failed copy

--- obrl/oblr-src-result-table-copy.user.js
+++ obrl/oblr-src-result-table-copy.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name          Sim Racer Hub Race Result Table Copy
-// @version       0.2.0
+// @version       0.3.0
 // @description   Copies the result table for easy paste into Excel
 // @author        Gavin Adams
 // @namespace     OBRL
@@ -16,60 +16,67 @@
   "use strict";
 
   function copyTable() {
-    const tables = document.querySelectorAll(".react-table");
-    let table = null;
-    let table_text = null;
-    // Raw header is FIN to the first \n\n, body is the rest
-    const re = /^(?<header>FIN.*\n\n)(?<body>.*)$/s;
-    let header = "";
-    let body = "";
-    switch (tables.length) {
-      case 0:
-        alert(`There are no HTML tables.`);
-        break;
-      case 1:
-        table = tables[0];
-        break;
-      case 3:
-        // 2024-02-16 - SRH shows 3 tables today. This may change.
-        table = tables[0];
-        break;
-      default:
-        if (table === null) {
-          const choice = parseInt(
-            prompt(
-              `There are ${tables.length} tables and should only have 3, so something has changed. What table (1 should be the default) would you like to load?`
-            )
-          );
+      const tables = document.querySelectorAll(".react-table");
+      let table = null;
+      let table_text = null;
+      // Raw header is FIN to the first \n\n, body is the rest
+      const re = /^(?<header>FIN.*dd\n\n)(?<body>.*)$/s;
+      let header = "";
+      let body = "";
+      switch (tables.length) {
+          case 0:
+              alert(`There are no HTML tables.`);
+              break;
+          case 1:
+              table = tables[0];
+              break;
+          case 3:
+              // 2024-02-16 - SRH shows 3 tables today. This may change.
+              table = tables[0];
+              break;
+          default:
+              if (table === null) {
+                  const choice = parseInt(
+                      prompt(
+                          `There are ${tables.length} tables and should only have 3, so something has changed. What table (1 should be the default) would you like to load?`
+          )
+        );
           if (choice >= 1 && choice <= tables.length) {
-            table = tables[choice - 1];
+              table = tables[choice - 1];
           } else {
-            alert(`Number is out of range, please try again.`);
-            copyTable();
-            return;
+              alert(`Number is out of range, please try again.`);
+              copyTable();
+              return;
           }
-        }
-    }
+      }
+  }
     var x = table.outerText;
     // console.log(JSON.stringify(x));
     table_text = x.match(re);
-    header = table_text.groups.header;
-    body = table_text.groups.body;
-    // header -- replace \n with ' '
-    header = header.replaceAll(/(\w)\n(\w)/g, "$1 $2");
-    // header -- replace \n\t\n with \t
-    header = header.replaceAll(/\n\t\n/g, "\t");
-    // header -- strip final \n
-    header = header.substring(0, header.length - 1);
-    console.log(JSON.stringify(header));
-    // body -- strip final \t
-    body = body.substring(0, body.length - 1);
+    // check if the regex worked
+    if (table_text === null) {
+        alert(
+            `I could't import the data based on the layout. Soemething has changed, please contact Gavin to review.`
+          );
+  } else {
+      header = table_text.groups.header;
+      body = table_text.groups.body;
+      // header -- replace \n with ' '
+      header = header.replaceAll(/(\w)\n(\w)/g, "$1 $2");
+      // header -- replace \n\t\n with \t
+      header = header.replaceAll(/\n\t\n/g, "\t");
+      // header -- strip final \n
+      header = header.substring(0, header.length - 1);
+      console.log(JSON.stringify(header));
+      // body -- strip final \t
+      body = body.substring(0, body.length - 1);
 
-    GM_setClipboard("".concat(header, body), "text");
-    alert(
-      `Table is probably copied (well, without Fisch's details, lol). Now paste it into spreadsheet.`
-    );
+      GM_setClipboard("".concat(header, body), "text");
+      alert(
+          `Table is copied, now paste it into spreadsheet.`
+  );
   }
+}
 
   GM_registerMenuCommand("Copy table for spreadsheet", copyTable);
 })();
